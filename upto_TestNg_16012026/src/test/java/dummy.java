4d7c6be5df73b7d686aaee9dff490d10d36a2160
DELL <DELL@DESKTOP-2K2FBPT>
2026-01-16 12:31:21 +0530

    2nd automation engg file is added

--- upto_TestNg_16012026/src/test/java/dummy.java
+++ upto_TestNg_16012026/src/test/java/dummy.java
@@ -6,18 +6,7 @@ import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class dummy {
 public static void main(String[] args) {
-	ExtentSparkReporter spark =new ExtentSparkReporter("c:/path.html");
-	spark.config().setDocumentTitle("VTIGER SUITE RESULTS");
-	spark.config().setReportName("MODULES REPORTSSSS.....");
-	spark.config().setTheme(Theme.DARK);
 	
-	ExtentReports report=new ExtentReports();
-	report.attachReporter(spark);
-	report.setSystemInfo("OS", "WIN-10");
-	report.setSystemInfo("BROWSER", "CHROME-100");
-	System.out.println("***********************");
-	ExtentTest test = null;
-	//test.log(Status.INFO);
 
 }
 }
---
+++ upto_TestNg_16012026/src/test/java/secondAutomationengg.java
@@ -0,0 +1,8 @@
+
+public class secondAutomationengg {
+public static void main(String[] args) {
+	
+	
+	System.out.println("22222222222 automation engineer working");
+}
+}
